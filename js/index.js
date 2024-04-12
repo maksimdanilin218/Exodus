a387f167101d46b0705da90ed5b173609f4a3c9c
maksimdanilin218 <maksimdanilin218@gmaul.com>
2024-04-12 15:54:44 +0400

    new commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/*Бургер кнопка*/ 
+document.querySelector('.burger__btn').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.nav__info').classList.toggle('open')
+})
+/*анимация header заголовка*/ 
+
+
+/*анимация про прокрутке*/ 
+/*gym__grid*/ 
+
+
+
+/*тригер для срабатывания анимацтт подсчета*/ 
+
+/*анимация счетчика*/ 
+
