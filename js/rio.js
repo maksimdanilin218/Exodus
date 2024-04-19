b8b9c013430aca02433958df732b75bb1157746b
maksimdanilin218 <maksimdanilin218@gmaul.com>
2024-04-19 20:53:41 +0400

    new commit trener

--- js/rio.js
+++
@@ -1,5 +0,0 @@
-/*Бургер кнопка*/ 
-document.querySelector('.burger__btn').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.querySelector('.nav__info').classList.toggle('open')
-})
